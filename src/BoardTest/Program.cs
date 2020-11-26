8bfe558dedf2dbc9dfb1b8b467602e7fe1db9dcb
Don Cross <cosinekitty@gmail.com>
2020-11-26 17:02:09 -0500

    First commit of my new C# chess engine for .NET Core 5.0+.

---
+++ src/BoardTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoardTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
