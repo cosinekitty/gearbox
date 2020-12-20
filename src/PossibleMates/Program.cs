a0125aa779ffae96476b0d6c1fbcd344db81abde
Don Cross <cosinekitty@gmail.com>
2020-12-20 14:22:53 -0500

    Updated PossibleMates program to allow passing in configuration on command line.

--- src/PossibleMates/Program.cs
+++ src/PossibleMates/Program.cs
@@ -5,16 +5,87 @@ namespace PossibleMates
 {
     class Program
     {
+        const string Usage = @"
+USAGE:  PossibleMates [--terse] config
+
+Where config is one or more of the following characters:
+
+    P = White Pawn
+    N = White Knight
+    B = White Bishop
+    R = White Rook
+    Q = White Queen
+
+    p = Black Pawn
+    n = Black Knight
+    b = Black Bishop
+    r = Black Rook
+    q = Black Queen
+
+This program tries every legal combination of the specified
+pieces on the board, with Black to move. It then finds all
+positions where Black is checkmated.
+
+By default, all the FEN positions are printed, followed by
+the number of checkmates.
+
+With the --terse option, only the number of checkmates is printed.
+
+";
+
         static readonly int[] OffsetTable = MakeOffsetTable();
+        static bool TerseMode;
 
         static int Main(string[] args)
         {
-            // Find all legal board positions with combinations
-            // of two kings, zero or one white bishop/knight,
-            // and zero or one black bishop/knight.
-            int count = FindCheckmatePositions(Square.WQ);
-            Console.WriteLine("Found {0} checkmates.", count);
+            string config;
+            if (args.Length == 1)
+            {
+                TerseMode = false;
+                config = args[0];
+            }
+            else if (args.Length == 2 && args[0] == "--terse")
+            {
+                TerseMode = true;
+                config = args[1];
+            }
+            else
+            {
+                Console.WriteLine(Usage);
+                return 1;
+            }
+
+            Square[] nonKingPieces = ParseNonKingPieces(config);
+            int count = FindCheckmatePositions(nonKingPieces);
+            if (TerseMode)
+                Console.WriteLine("{0}", count);
+            else
+                Console.WriteLine("Found {0} checkmates for Kk{1}.", count, config);
             return 0;
+        }
+
+        static Square[] ParseNonKingPieces(string config)
+        {
+            var pieces = new Square[config.Length];
+            for (int i=0; i < config.Length; ++i)
+            {
+                switch (config[i])
+                {
+                    case 'P':   pieces[i] = Square.WP;  break;
+                    case 'N':   pieces[i] = Square.WN;  break;
+                    case 'B':   pieces[i] = Square.WB;  break;
+                    case 'R':   pieces[i] = Square.WR;  break;
+                    case 'Q':   pieces[i] = Square.WQ;  break;
+                    case 'p':   pieces[i] = Square.BP;  break;
+                    case 'n':   pieces[i] = Square.BN;  break;
+                    case 'b':   pieces[i] = Square.BB;  break;
+                    case 'r':   pieces[i] = Square.BR;  break;
+                    case 'q':   pieces[i] = Square.BQ;  break;
+                    default:
+                        throw new ArgumentException("Invalid non-king piece character: " + config[i]);
+                }
+            }
+            return pieces;
         }
 
         static int KingDistance(int ofs1, int ofs2)
@@ -74,7 +145,8 @@ namespace PossibleMates
 
             if (board.IsValidPosition() && board.IsCheckmate())
             {
-                Console.WriteLine(board.ForsythEdwardsNotation());
+                if (!TerseMode)
+                    Console.WriteLine(board.ForsythEdwardsNotation());
                 return 1;
             }
 
